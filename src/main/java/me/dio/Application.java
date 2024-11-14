5464e59b736cfa72be04eeea70d931788befbfdf
FlaGit1 <flaviano.oliveira@globalhitss.com.br>
2024-11-14 02:12:05 -0300

    Implementação da camada de domínio

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -3,12 +3,12 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//teste2
+
 @SpringBootApplication
-public class SantanderDevWeek2023Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeek2023Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
